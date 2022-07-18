8c0eb461990cbfa484bc09c995a43af14762109d
Tarek <103357698+TarekBouaziz1994@users.noreply.github.com>
2022-07-18 10:58:53 +0200

    Add files via upload

---
+++ MiniProjetTarekBouaziz/test.js
@@ -0,0 +1,12 @@
+var score = 0;
+
+    function myFunction0() {
+      alert("Wrong answer , your score is " + 0);
+      
+    }
+  
+    function myFunction1() {
+      score = score + 10;
+      alert("Congratulation, correct answer, your score is now " + score);
+      
+    }
